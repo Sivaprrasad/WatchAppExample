db75fc7eba2dc4b0ccb46c5c1b70da41cf1c5d91
Sivaprrasad Uppalapati <usivaprasad95@gmail.com>
2019-10-15 11:28:34 -0400

    Initial Commit

---
+++ WatchApp WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchApp WatchKit Extension
+//
+//  Created by Uppalapati SivaPrrasad on 15/10/19.
+//  Copyright © 2019 Uppalapati SivaPrrasad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchApp WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchApp WatchKit Extension
+//
+//  Created by Uppalapati SivaPrrasad on 15/10/19.
+//  Copyright © 2019 Uppalapati SivaPrrasad. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
